99138c1e5a62741dd2c56825190bd883f5f2b84e
Lee Doughty <leedoughtydesign@gmail.com>
2019-01-17 10:26:35 +0000

    Add gatsby

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+// export default () => <div>Hello Lee!</div>
+
+export default () => (
+  <div style={{ color: `blue` }}>
+  <h1>Hello Gatsby!</h1>
+  <p>what a world</p>
+  <img src="http://leedoughty.io/img/lee.jpg" alt="pic of lee"/>
+  </div>
+)
