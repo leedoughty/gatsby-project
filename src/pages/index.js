8dbffd2f2dca23139fa11e5bd70b356bcab65b7e
Lee Doughty <leedoughtydesign@gmail.com>
2019-01-21 21:41:35 +0000

    Add emotion button

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,19 +2,23 @@ import React from "react"
 import { Link } from "gatsby"
 import Header from "../components/header"
 import styled from "styled-components"
+import EmotionButton from "../components/emotion-button"
 
 const Container = styled.div`
   display: flex;
-  align-items: center;
-  justify-content: space-between;
+  /* align-items: center; */
+  justify-content: left;
 `
 
 export default () => (
   <Container>
-    <div style={{ color: `blue` }}>
-      <Header headerText="cool ascii art" />
-      <Link to="/contact/">Contact</Link>
-      <Link to="/about/">About</Link>
-    </div>
+    <Header headerText="cool ascii art" />
+    <Link to="/contact/">Contact</Link>
+    <Link to="/about/">About</Link>
+    <EmotionButton emotion="happy" />
+    <EmotionButton emotion="sad" />
+    <EmotionButton emotion="neutral" />
+    <EmotionButton emotion="angry" />
+    <EmotionButton emotion="suprised" />
   </Container>
 )
