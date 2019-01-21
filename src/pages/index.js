f8b8a9d99bfd56a6a6a49b67f70dc84bfe8238dd
Lee Doughty <leedoughtydesign@gmail.com>
2019-01-21 21:25:25 +0000

    Update styling for layout on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,18 @@ import { Link } from "gatsby"
 import Header from "../components/header"
 import styled from "styled-components"
 
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+
 export default () => (
-  <div style={{ color: `blue` }}>
-    <Link to="/contact/">Contact</Link>
-    <Link to="/about/">About</Link>
-    <Header headerText="cool ascii art" />
-    <p>what a world</p>
-    <img src="http://leedoughty.io/img/lee.jpg" alt="pic of lee"/>
-  </div>
+  <Container>
+    <div style={{ color: `blue` }}>
+      <Header headerText="cool ascii art" />
+      <Link to="/contact/">Contact</Link>
+      <Link to="/about/">About</Link>
+    </div>
+  </Container>
 )
