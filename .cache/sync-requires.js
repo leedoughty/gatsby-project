bff626d49fe4ea4cd5c85ba9d25df6522f3ecbe0
Lee Doughty <leedoughtydesign@gmail.com>
2019-01-22 21:10:50 +0000

    Remove contact page

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -7,7 +7,6 @@ const preferDefault = m => m && m.default || m
 exports.components = {
   "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/leedoughty/Desktop/gatsby-project/.cache/dev-404-page.js"))),
   "component---src-pages-about-js": hot(preferDefault(require("/Users/leedoughty/Desktop/gatsby-project/src/pages/about.js"))),
-  "component---src-pages-contact-js": hot(preferDefault(require("/Users/leedoughty/Desktop/gatsby-project/src/pages/contact.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/leedoughty/Desktop/gatsby-project/src/pages/index.js")))
 }
 
