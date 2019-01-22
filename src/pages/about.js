0e07000449b36e4de5a93388ffda71c16491cabc
Lee Doughty <leedoughtydesign@gmail.com>
2019-01-22 21:14:33 +0000

    Update information on about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,9 +3,10 @@ import { Link } from "gatsby"
 import Header from "../components/header"
 
 export default () => (
-  <div style={{ color: `teal` }}>
+  <div>
     <Link to="/">Home</Link>
     <Header headerText="About" />
     <p>welcome to the world of japanese ascii art</p>
+    <p>created by </p><a href="https://www.leedoughty.com">leedoughty</a>
   </div>
 )
